9e990183d368d9347c7350068dfd4ca64b1b9566
Balazss01 <balazsmagyar301@gmail.com>
2026-03-18 18:10:39 +0100

    TagokController Crud

---
+++ SmartGym/Controllers/TagokController.cs
@@ -0,0 +1,78 @@
+﻿using GymWebApiBackend.Data;
+using GymWebApiBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymWebApiBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TagokController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TagokController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var tagok = await _context.Tagok.ToListAsync();
+            return Ok(tagok);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var tag = await _context.Tagok.FindAsync(id);
+
+            if (tag == null)
+                return NotFound();
+
+            return Ok(tag);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Tag tag)
+        {
+            _context.Tagok.Add(tag);
+            await _context.SaveChangesAsync();
+
+            return Ok(tag);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Tag updatedTag)
+        {
+            var tag = await _context.Tagok.FindAsync(id);
+
+            if (tag == null)
+                return NotFound();
+
+            tag.Vezeteknev = updatedTag.Vezeteknev;
+            tag.Keresztnev = updatedTag.Keresztnev;
+            tag.SzuletesiDatum = updatedTag.SzuletesiDatum;
+            tag.Aktiv = updatedTag.Aktiv;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(tag);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var tag = await _context.Tagok.FindAsync(id);
+
+            if (tag == null)
+                return NotFound();
+
+            _context.Tagok.Remove(tag);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}   
