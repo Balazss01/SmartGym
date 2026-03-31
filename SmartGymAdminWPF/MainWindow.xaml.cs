862b90aaa1047d490024b68eac01095fd1cf3f23
Imregaborka07 <imregaborka08@gmail.com>
2026-03-31 10:15:00 +0200

    SmartGymAdminWPF admin panel alap DataGrid és API kapcsolattal

---
+++ SmartGymAdminWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using SmartGymAdminWPF.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SmartGymAdminWPF
+{
+    public partial class MainWindow : Window
+    {
+        HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _ = LoadUsers();
+        }
+
+        private async Task LoadUsers()
+        {
+            try
+            {
+                var response = await client.GetAsync("https://localhost:5001/api/users");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    var users = JsonSerializer.Deserialize<List<User>>(json, new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+
+                    UsersGrid.ItemsSource = users;
+                }
+                else
+                {
+                    MessageBox.Show("API hiba");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Backend nem fut!");
+            }
+        }
+    }
+}
